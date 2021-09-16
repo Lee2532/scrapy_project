9d23470542c88df64c48583afb35de40857d139e
Lee2532 <seungoh0423@gmail.com>
2021-09-16 22:21:14 +0900

    게시글 링크를 타고 들어가서 해당 데이터 수집하는 로직

--- navercrawler/navercrawler/spiders/navernewscrawler.py
+++ navercrawler/navercrawler/spiders/navernewscrawler.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from navercrawler.items import NavercrawlerItem, NaverCrawlerDataClasee, CustomItem
 
 class ToScrapeCSSSpider(scrapy.Spider):
     name = "navernews"
@@ -9,27 +9,29 @@ class ToScrapeCSSSpider(scrapy.Spider):
     ]
 
     def start_requests(self):
-        for i in range(10):
+        for i in range(1):
             target_url = f'https://news.naver.com/main/list.naver?mode=LSD&mid=sec&sid1=001&date=20210915&page={i}'
             yield scrapy.Request(url=target_url, callback=self.parse)
 
     def parse(self, response):
         for data in response.css("#main_content > div.list_body.newsflash_body > ul.type06_headline > li"):
+            item = NavercrawlerItem()
             target_url = data.css('dt:nth-child(2) > a::attr(href)').get()
             title = data.css(" dl > dd > span.lede::text").get()
             author = data.css("dl > dd > span.writing::text").get()
             preview = data.css("dt:nth-child(2) > a::text").get()
 
+            item['title'] = title
+            item['author'] = author
+            item['preview'] = preview
+            item['target_url'] = target_url
 
+            yield item
             yield scrapy.Request(target_url, callback=self.parse_post)
 
-            # yield {
-            #     'title': title,
-            #     'author': author,
-            #     'target_url' : target_url,
-            #     'preview': preview.strip()
-            # }
 
     def parse_post(self, response):
-        print(response)
-        pass
+        title = response.css('h3#articleTitle::text').get()
+        target_url = response.url
+
+        yield CustomItem(title, target_url)
